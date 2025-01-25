c8e48bd4c66bf456a877ae0b0967c4fe4d4bc50b
Marcelly Freitas <marcelly.moraes@sga.pucminas.br>
2025-01-25 08:05:53 -0300

    Adicionar arquivos de projeto.

---
+++ DotnetRoomReservation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetRoomReservation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
