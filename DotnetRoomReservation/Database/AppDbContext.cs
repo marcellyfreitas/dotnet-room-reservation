beb6e343dd3563f0e58f94eb3ae7231bd77ba4b3
Marcelly Freitas <marcelly.moraes@sga.pucminas.br>
2025-01-25 08:53:02 -0300

    feat: adicionando dbcontext

---
+++ DotnetRoomReservation/Database/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using DotnetRoomReservation.Models;
+
+namespace DotnetRoomReservation.Database;
+
+public class AppDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var configuration = optionsBuilder.Options.FindExtension<CoreOptionsExtension>()?.ApplicationServiceProvider?.GetService<IConfiguration>();
+
+            if (configuration == null)
+            {
+                throw new Exception("Arquivo de configuração não encontrado.");
+            }
+
+            optionsBuilder.UseSqlite(_configuration.GetConnectionString("SQLiteConnection"));
+        }
+    }
+
+
+    public DbSet<Administrador> Administradores { get; set; }
+}
